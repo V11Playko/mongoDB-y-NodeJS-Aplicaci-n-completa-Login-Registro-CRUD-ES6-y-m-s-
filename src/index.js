5d7b91a92a3403eaec8785aa47e438bbc66dbb5b
V11Playko <heinnervega20@gmail.com>
2022-05-16 08:32:48 -0500

    Se hizo config de vistas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const path = require("path");
 
 // Initializations
 const app = express();
 
 // Settings
 app.set("port", process.env.PORT || 3000);
+
+// Views
+app.set("views", path.join(__dirname, "views"));
+
+app.engine(
+  ".hbs",
+  engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+  })
+);
+app.set("view engine", ".hbs");
 
 // Middlewares
 
