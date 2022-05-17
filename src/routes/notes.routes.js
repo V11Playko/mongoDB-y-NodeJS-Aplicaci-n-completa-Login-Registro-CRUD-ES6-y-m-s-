e88bbfac5870d9d57c9aa1fd173b0e06be7d2e24
V11Playko <heinnervega20@gmail.com>
2022-05-17 07:53:26 -0500

    Ruta de notes y views edit

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -2,7 +2,39 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/notes", (req, res) => {
-  res.send("Notas del servidor");
+  res.render("notes/all-notes");
+});
+
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-note");
+});
+
+router.post("/notes/new-note", async (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+  if (!title) {
+    errors.push({ text: "Please Write a Title." });
+  }
+  if (!description) {
+    errors.push({ text: "Please Write a Description" });
+  }
+  if (errors.length > 0) {
+    res.render("notes/new-note", {
+      errors,
+      title,
+      description,
+    });
+  } else {
+    const newNote = new Note({ title, description });
+    newNote.user = req.user.id;
+    await newNote.save();
+    req.flash("success_msg", "Note Added Successfully");
+    res.redirect("/notes");
+  }
+});
+
+router.put("/notes/edit-note/:id", (req, res) => {
+  res.render("./notes/edit-note");
 });
 
 module.exports = router;
