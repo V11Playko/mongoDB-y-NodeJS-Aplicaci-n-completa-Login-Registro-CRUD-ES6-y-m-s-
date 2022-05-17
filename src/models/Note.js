146f48aa5a103228fd5a55176fed286aa0049c54
V11Playko <heinnervega20@gmail.com>
2022-05-17 15:25:17 -0500

    se hizo el modelo de las notas

---
+++ src/models/Note.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const NoteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Note", NoteSchema);
