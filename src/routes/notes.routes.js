5b2bcf50a8805e90b7f35170fc1ba0d6d48d5fa9
V11Playko <heinnervega20@gmail.com>
2022-05-18 06:55:24 -0500

    Fixers a notes.routes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -25,38 +25,38 @@ router.post("/notes/new-note", async (req, res) => {
   } else {
     const newNote = new Note({ title, description });
     await newNote.save();
-    req.flash("succes_msg", 'Note added succesfully')
+    req.flash("succes_msg", "Note added succesfully");
     res.redirect("/notes");
   }
 });
 
-router.get("/notes", (req, res) => {
+router.get("/notes", async (req, res) => {
   const notes = await Note.find({ user: req.user.id })
     .sort({ date: "desc" })
     .lean();
   res.render("notes/all-notes", { notes });
 });
 
-router.get("/notes/edit/:id", (req, res) => {
+router.get("/notes/edit/:id", async (req, res) => {
   const note = await Note.findById(req.params.id).lean();
   if (note.user != req.user.id) {
     req.flash("error_msg", "Not Authorized");
     return res.redirect("/notes");
   }
   res.render("notes/edit-note", { note });
-})
+});
 
 router.put("/notes/edit-note/:id", async (req, res) => {
   const { title, description } = req.body;
   await Note.findByIdAndUpdate(req.params.id, { title, description });
   req.flash("success_msg", "Note Updated Successfully");
   res.redirect("/notes");
-})
+});
 
-router.delete('/notes/delete/:id', (req, res) => {
+router.delete("/notes/delete/:id", async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
   req.flash("success_msg", "Note Deleted Successfully");
   res.redirect("/notes");
-})
+});
 
 module.exports = router;
