a9935d2393f3ba921c6d362023b4c0233beee42c
V11Playko <heinnervega20@gmail.com>
2022-05-18 17:14:37 -0500

    Haciendo ajuste a las vistas

--- src/database.js
+++ src/database.js
@@ -2,7 +2,9 @@ const { connect } = require("mongoose");
 
 (async () => {
   try {
-    const db = await connect(MONGODB_URI);
+    const db = await connect(
+      "mongodb+srv://playko:komodor11@apidb.lldrd.mongodb.net/apiNotes"
+    );
     console.log("Db connectect to", db.connection.name);
   } catch (error) {
     console.error(error);
