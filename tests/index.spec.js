cf219adbc32d154e197628eac95d799eff610b87
V11Playko <heinnervega20@gmail.com>
2022-05-20 15:49:05 -0500

    test correjido

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,9 +1,30 @@
-const app = require("../src/routes/index.routes.js");
+const app = require("../src/routes/index.routes");
 const request = require("supertest");
+
+describe("GET /", () => {
+  test("should respond with a 200 status code", async () => {
+    const response = request(app)
+      .get("/")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "15")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
+    console.log(response);
+  });
+});
 
 describe("GET /about", () => {
   test("should respond with a 200 status code", async () => {
-    const response = await request(app).get("/about").send();
+    const response = request(app)
+      .get("/about")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "15")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
     console.log(response);
   });
 });
