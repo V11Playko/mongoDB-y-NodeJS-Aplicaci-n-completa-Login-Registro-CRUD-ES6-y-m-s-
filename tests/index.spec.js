b165ad4bc5be6adb7fe999e79d7f98fee2a56355
V11Playko <heinnervega20@gmail.com>
2022-05-20 16:31:24 -0500

    Test a note

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,9 +1,12 @@
 const app = require("../src/routes/index.routes");
+const nApp = require("../src/routes/notes.routes");
 const request = require("supertest");
+
+// Pruebas de conexion a index.routes
 
 describe("GET /", () => {
   test("should respond with a 200 status code", async () => {
-    const response = request(app)
+    request(app)
       .get("/")
       .expect("Content-Type", /json/)
       .expect("Content-Length", "15")
@@ -11,13 +14,12 @@ describe("GET /", () => {
       .end(function (err, res) {
         if (err) throw err;
       });
-    console.log(response);
   });
 });
 
 describe("GET /about", () => {
   test("should respond with a 200 status code", async () => {
-    const response = request(app)
+    request(app)
       .get("/about")
       .expect("Content-Type", /json/)
       .expect("Content-Length", "15")
@@ -25,6 +27,87 @@ describe("GET /about", () => {
       .end(function (err, res) {
         if (err) throw err;
       });
-    console.log(response);
   });
 });
+
+// Pruebas de conexion a notes.routes
+
+describe("GET /notes/add", () => {
+  test("should respond with a 200 status code", async () => {
+    request(app)
+      .get("/notes/add")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "15")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
+  });
+});
+
+describe("POST /notes/new-note", () => {
+  test("should respond with a 200 status code", async () => {
+    request(app)
+      .post("/notes/new-note")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "15")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
+  });
+});
+
+describe("GET /notes", () => {
+  test("should respond with a 200 status code", async () => {
+    request(app)
+      .get("/notes")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "15")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
+  });
+});
+
+describe("GET /notes/edit/:id", () => {
+  test("should respond with a 200 status code", async () => {
+    request(app)
+      .get("/notes/edit/:id")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "15")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
+  });
+});
+
+describe("PUT /notes/edit-note/:id", () => {
+  test("should respond with a 200 status code", async () => {
+    request(app)
+      .put("/notes/edit-note/:id")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "15")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
+  });
+});
+
+describe("DEL /notes/delete/:id", () => {
+  test("should respond with a 200 status code", async () => {
+    request(app)
+      .del("/notes/delete/:id")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "15")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
+  });
+});
+
+//
